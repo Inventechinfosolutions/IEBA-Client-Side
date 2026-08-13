1bb53d86d513514cbd3cfb44eba89c498f948848
Srujan <srujan@Admins-MacBook-Pro-4.local>
2026-08-13 12:52:38 +0530

    implemeneted the deBounce for components

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,3 +1,12 @@
+import { useEffect, useState } from "react"
+
 export function useDebounce<T>(value: T): T {
-  return value
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value)
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [value])
+  return debouncedValue
 }
